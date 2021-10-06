40821564fde3433a3310fe175b4c14d1b740c570
samuelcj <91881577+samuelcj@users.noreply.github.com>
2021-10-06 10:56:07 +0100

    Add files via upload

---
+++ collections.py
@@ -0,0 +1,23 @@
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+myFruitList[2] = "orange"
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+myFinalAnswerTuple = ("Apple", "Banana", "Pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+myFavoriteFruitDictionary = {
+    "Akua" : "apple",
+    "Saanvi" : "banana",
+    "Paulo" : "pineapple"
+   }
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
---
+++ hello-world.py
@@ -0,0 +1,2 @@
+print("Hello, world")
+print("Python has three numeric types: int, float, and complex")
